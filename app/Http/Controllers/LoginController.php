5b6e8eca2d44df98d15f75cfcdc69b278d7a2bb8
Didin Yahya <didinnuryahya@gmail.com>
2022-12-19 22:49:03 +0700

    add feature auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Admin;
+use Hash;
+use Validator;
+use Auth;
+
+class LoginController extends Controller
+{
+    public function userDashboard()
+    {
+        $success =  [
+            "Message" => 'Welcome to User Dashboard',
+            "Name" => Auth::user()->name
+        ];
+
+        return response()->json($success, 200);
+    }
+
+    public function adminDashboard()
+    {
+        $users = User::all();
+        $success = [
+            "Message" => 'Welcome to Admin Dashboard',
+            "Data_user" => $users
+        ];
+
+        return response()->json($success, 200);
+    }
+
+    public function userLogin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()->all()]);
+        }
+
+        if(auth()->guard('user')->attempt(['email' => request('email'), 'password' => request('password')])){
+git init
+            config(['auth.guards.api.provider' => 'user']);
+            
+            $user = User::select('users.*')->find(auth()->guard('user')->user()->id);
+            $success =  $user;
+            $success['token'] =  $user->createToken('appToken',['user'])->accessToken; 
+
+            return response()->json($success, 200);
+        }else{ 
+            return response()->json(['error' => ['Email and Password are Wrong.']], 200);
+        }
+    }
+
+    public function adminLogin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()->all()]);
+        }
+
+        if(auth()->guard('admin')->attempt(['email' => request('email'), 'password' => request('password')])){
+
+            config(['auth.guards.api.provider' => 'admin']);
+            
+            $admin = admin::select('clients.*')->find(auth()->guard('admin')->user()->id);
+            $success =  $admin;
+            $success['token'] =  $admin->createToken('appToken',['admin'])->accessToken; 
+
+            return response()->json($success, 200);
+        }else{ 
+            return response()->json(['error' => ['Email and Password are Wrong.']], 200);
+        }
+    }
+}
---
+++ database/migrations/2022_12_19_114422_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->bigInteger('admin_id')->unsigned()->nullable();
+            $table->string('header');
+            $table->string('summmary');
+            $table->text('content');
+            $table->string('slug');
+            $table->boolean('status');
+            $table->boolean('is_delete');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
